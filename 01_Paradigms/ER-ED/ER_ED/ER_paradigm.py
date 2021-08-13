7bb8b17fb953693f3083e89d5896521a608cff61
JZerna <85111801+JZerna@users.noreply.github.com>
2021-08-13 08:24:13 +0200

    Removed double initialization of fixcross routine

--- 01_Paradigms/ER-ED/ER_ED/ER_paradigm.py
+++ 01_Paradigms/ER-ED/ER_ED/ER_paradigm.py
@@ -226,15 +226,6 @@ image_reg = visual.ImageStim(
     flipHoriz=False, flipVert=False,
     texRes=128.0, interpolate=True, depth=0.0)
 
-# Initialize components for Routine "fixcross"
-fixcrossClock = core.Clock()
-text = visual.TextStim(win=win, name='text',
-    text='+',
-    font='Open Sans',
-    pos=(0, 0), height=0.1, wrapWidth=None, ori=0.0, 
-    color='black', colorSpace='rgb', opacity=None, 
-    languageStyle='LTR',
-    depth=0.0);
 
 # Initialize components for Routine "Slider_Arousal"
 Slider_ArousalClock = core.Clock()
