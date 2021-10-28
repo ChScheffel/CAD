169267e9221c84c60324c333664e878202a4d45b
JZerna <85111801+JZerna@users.noreply.github.com>
2021-10-28 14:57:07 +0200

    changed bufferscreen time

--- 01_Paradigms/COG-ED/t1_Paradigm.py
+++ 01_Paradigms/COG-ED/t1_Paradigm.py
@@ -1056,7 +1056,7 @@ for edx in EDrounds:
         
         # ------Prepare to start Routine "bufferscreen"-------
         continueRoutine = True
-        routineTimer.add(2.000000)
+        routineTimer.add(0.500000)
         # update component parameters for each repeat
         bufferscreenkey.keys = []
         bufferscreenkey.rt = []
