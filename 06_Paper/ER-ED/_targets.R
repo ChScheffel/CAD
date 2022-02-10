d975c5d70253b2da0c730564c5c1ec33608d7c1b
Christoph Scheffel <77979289+ChScheffel@users.noreply.github.com>
2022-02-10 16:04:57 +0100

    updates on targets

--- 06_Paper/ER-ED/_targets.R
+++ 06_Paper/ER-ED/_targets.R
@@ -1,5 +1,9 @@
 library(targets)
 library(tarchetypes)
+library(here)
+
+# set top level directory to source file
+here::i_am("flag_project_root_CERED.txt")
 
 # Set target-specific options such as packages.
 tar_option_set(packages = "tidyverse")
@@ -7,8 +11,12 @@ options(tidyverse.quiet = TRUE)
 
 # End this file with a list of target objects.
 list(
-  tar_target(whole.paper, "Manuscript_ERED_Stage1.Rmd"),
-  tar_target(intro, "TheoreticalBackground.Rmd"),
-  tar_target(method, "Method.Rmd"),
-  tar_target(Quest, "Questionnaires.R")
+  tar_target(intro, "TheoreticalBackground.Rmd", format = "file"),
+  tar_target(method, "Method.Rmd", format = "file"),
+  tar_target(Quest, "Questionnaires.R", format = "file"),
+  tar_target(raw_data_file, here("04_RawData", "pilot", "CERED_DATA.csv"), format = "file"),
+  tar_target(raw_data, read.csv(here("04_RawData", "pilot", "CERED_DATA.csv"),
+                                 stringsAsFactors = FALSE, header = TRUE,
+                                 na.strings = c("", "NA"))),
+  tar_render(whole.paper, "Manuscript_ERED_Stage1.Rmd")
 )
