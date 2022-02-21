4f1d19e8b3c503680a567bcc17f74f6563457374
Christoph Scheffel <77979289+ChScheffel@users.noreply.github.com>
2022-02-21 10:51:59 +0100

    added library renv

--- 06_Paper/ER-ED/Analyes_ERED.r
+++ 06_Paper/ER-ED/Analyes_ERED.r
@@ -17,15 +17,10 @@ library(here)       # to set directories without defined paths
 # set top level directory to source file
 here::i_am("flag_project_root_CERED.txt")
 
-library(papaja)     # APA conform output
-library(afex)       # for ANOVA
-library(emmeans)
-library(ggplot2)    # for nice plots
-library(dplyr)
-library(BayesFactor)
-library(effectsize)
-library(targets)    # to keep track of all relevant files
-library(readxl)     # import excel files
+library(renv)
+renv::activate(here("06_Paper","ER-ED"))
+renv::restore(here("06_Paper","ER-ED"))
+
 
 ##################### DATA IMPORT #######################
 
