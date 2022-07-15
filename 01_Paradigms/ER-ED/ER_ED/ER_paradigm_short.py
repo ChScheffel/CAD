7b122ec551d686c270bbf9a517a7c056df139d2f
Christoph Scheffel <77979289+ChScheffel@users.noreply.github.com>
2022-07-15 12:09:27 +0200

    fixed typo

--- 01_Paradigms/ER-ED/ER_ED/ER_paradigm_short.py
+++ 01_Paradigms/ER-ED/ER_ED/ER_paradigm_short.py
@@ -129,7 +129,7 @@ EDstratList = [1, 2,
 2, 3]
 
 # The steps in which the monetary values will be adapted in the ED part
-EDsteps = [1.00,1.00,0.50,0.25,0.13,0.06,0.03,0.02]
+EDsteps = [1.00,0.50,0.25,0.13,0.06,0.03,0.02]
 # The constant monetary value that will be assigned to the option that was not chosen in the 1€ vs 1€ comparison
 EDfix = [2.00]
 # Create array corresponding to rounds of effort discounting aka the list elements
